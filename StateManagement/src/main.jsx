454a3042c3ffe965d5efcc035d6ab854bb58db28
Minit <minitjain.work@gmail.com>
2025-08-14 23:04:06 +0530

    .

--- StateManagement/src/main.jsx
+++ StateManagement/src/main.jsx
@@ -1,16 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./TOPICS/useState/App";
+// import App from "./TOPICS/useState/App";
 import AppX from "./TOPICS/useReducer/App";
 import AppY from "./TOPICS/useMemo,UseCallBack/App.jsx";
 import AppZ from "./TOPICS/useEffect/App.jsx";
+import App from "./TOPICS/ReduxTemplate/App.jsx";
+import { Provider } from "react-redux";
+import store from "./TOPICS/ReduxTemplate/store";
 
 createRoot(document.getElementById("root")).render(
   <>
     {/* <App /> */}
     {/* <AppX /> */}
     {/* <AppY /> */}
-    <AppZ />
+    {/* <AppZ /> */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </>
 );
